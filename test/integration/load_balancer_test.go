9738c4782ad1aea2ee261ba1ff95fa102d565b64
kevin <kevin@novek.io>
2024-09-21 12:48:03 +0300

    feat: added integration tests

---
+++ test/integration/load_balancer_test.go
@@ -0,0 +1,60 @@
+package integration
+
+import (
+	"context"
+	"time"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/sdfpt05/go_load_balancer/v2/internal/domain"
+	"github.com/sdfpt05/go_load_balancer/v2/internal/infrastructure/loadbalancers"
+	"github.com/sdfpt05/go_load_balancer/v2/internal/usecases"
+	"github.com/sdfpt05/go_load_balancer/v2/internal/interfaces"
+	"github.com/sdfpt05/go_load_balancer/v2/internal/infrastructure/circuitbreaker"
+)
+
+func TestLoadBalancerIntegration(t *testing.T) {
+	backend1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Backend 1"))
+	}))
+	defer backend1.Close()
+	
+	backend2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Backend 2"))
+	}))
+	defer backend2.Close()
+	
+	servers := []*domain.Server{
+		{URL: mustParseURL(backend1.URL)},
+		{URL: mustParseURL(backend2.URL)},
+	}
+	
+	lb := loadbalancers.NewRoundRobin(servers)
+	cb := circuitbreaker.NewCircuitBreaker(5, 10*time.Second)
+	useCase := usecases.NewLoadBalancerUseCase(lb, cb)
+	
+	logger, _ := zap.NewDevelopment()
+	handler := interfaces.NewHTTPHandler(useCase, logger)
+	
+	server := httptest.NewServer(handler)
+	defer server.Close()
+	
+	resp, err := http.Get(server.URL)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	defer resp.Body.Close()
+	
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status OK, got %v", resp.Status)
+	}
+}
+
+func mustParseURL(rawURL string) *url.URL {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		panic(err)
+	}
+	return u
+}
---
+++ test/unit/round_robin_test.go
@@ -0,0 +1,38 @@
+package unit
+
+import (
+	"context"
+	"testing"
+
+	"github.com/sdfpt05/go_load_balancer/v2/internal/domain"
+	"github.com/sdfpt05/go_load_balancer/v2/internal/infrastructure/loadbalancers"
+)
+
+func TestRoundRobin(t *testing.T) {
+	servers := []*domain.Server{
+		{URL: mustParseURL("http://server1.com")},
+		{URL: mustParseURL("http://server2.com")},
+		{URL: mustParseURL("http://server3.com")},
+	}
+	
+	rr := loadbalancers.NewRoundRobin(servers)
+	
+	for i := 0; i < 6; i++ {
+		server, err := rr.NextServer(context.Background())
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		expectedURL := servers[i%3].URL
+		if server.URL != expectedURL {
+			t.Errorf("Expected server %s, got %s", expectedURL, server.URL)
+		}
+	}
+}
+
+func mustParseURL(rawURL string) *url.URL {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		panic(err)
+	}
+	return u
+}
