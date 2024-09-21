7f4ba7bd3d3ba0d8cdc56dd0c621a3a804f693a1
kevin <kevin@novek.io>
2024-09-21 12:41:46 +0300

    feat: decoupled server rate limiting from lb algos

--- internal/usecases/load_balancer.go
+++ internal/usecases/load_balancer.go
@@ -2,20 +2,32 @@ package usecases
 
 import (
 	"context"
-	"github.com/sdfpt05/go_load_balancer/v2/internal/domain"
 	"time"
+
+	"github.com/sdfpt05/go_load_balancer/v2/internal/domain"
+	"github.com/sdfpt05/go_load_balancer/v2/internal/infrastructure/circuitbreaker"
 )
 
 type LoadBalancerUseCase struct {
-	lb domain.LoadBalancer
+	lb            domain.LoadBalancer
+	circuitBreaker *circuitbreaker.CircuitBreaker
 }
 
-func NewLoadBalancerUseCase(lb domain.LoadBalancer) *LoadBalancerUseCase {
-	return &LoadBalancerUseCase{lb: lb}
+func NewLoadBalancerUseCase(lb domain.LoadBalancer, cb *circuitbreaker.CircuitBreaker) *LoadBalancerUseCase {
+	return &LoadBalancerUseCase{
+		lb:            lb,
+		circuitBreaker: cb,
+	}
 }
 
 func (uc *LoadBalancerUseCase) GetNextServer(ctx context.Context) (*domain.Server, error) {
-	return uc.lb.NextServer(ctx)
+	var server *domain.Server
+	err := uc.circuitBreaker.Execute(func() error {
+		var err error
+		server, err = uc.lb.NextServer(ctx)
+		return err
+	})
+	return server, err
 }
 
 func (uc *LoadBalancerUseCase) UpdateServerStatus(server *domain.Server) {
@@ -34,4 +46,16 @@ func (uc *LoadBalancerUseCase) StartHealthCheck(ctx context.Context, interval ti
 			uc.lb.HealthCheck(ctx)
 		}
 	}
+}
+
+func (uc *LoadBalancerUseCase) AddServer(server *domain.Server) error {
+	return uc.lb.AddServer(server)
+}
+
+func (uc *LoadBalancerUseCase) RemoveServer(url string) error {
+	return uc.lb.RemoveServer(url)
+}
+
+func (uc *LoadBalancerUseCase) GetServers() []*domain.Server {
+	return uc.lb.GetServers()
 }
